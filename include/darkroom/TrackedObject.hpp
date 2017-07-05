fd4b1f9dd9d3d2c343f3ef5f1a6c255fe0b5fda4
letrend <simon.trendel@tum.de>
2017-07-06 01:22:15 +0200

    amazing shit

--- include/darkroom/TrackedObject.hpp
+++ include/darkroom/TrackedObject.hpp
@@ -37,6 +37,8 @@
 #include "darkroom/Sensor.hpp"
 #include "darkroom/PoseMinimizer.hpp"
 
+#include <common_utilities/rviz_visualization.hpp>
+
 // Converts degrees to radians.
 #define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
 // Converts radians to degrees.
@@ -56,14 +58,9 @@
 
 using namespace std;
 
-struct COLOR{
-    COLOR(float r, float g, float b, float a):r(r),g(g),b(b),a(a){};
-    float r,g,b,a;
-};
-
 static vector<int> DEFAULT_VECTOR;
 
-class TrackedObject {
+class TrackedObject:public rviz_visualization {
 public:
     TrackedObject();
 
@@ -235,66 +232,6 @@ private:
      * @return a unique id
      */
     int getMessageID(int type, int sensor, bool lighthouse = false);
-
-    /**
-     * Publishes a mesh visualization marker
-     * @param pos at this position
-     * @param orientation with this orientation
-     * @param modelname name of the mesh.dae
-     * @param frame in this frame
-     * @param ns namespace
-     * @param message_id unique id
-     * @param duration in seconds
-     */
-    void publishMesh(Vector3d &pos, Vector4d& orientation, const char* modelname,
-                                    const char *frame, const char *ns, int message_id, int duration);
-
-    /**
-     * Publishes a sphere visualization marker
-     * @param pos at this positon
-     * @param frame in this frame
-     * @param ns namespace
-     * @param message_id a unique id
-     * @param rgda rgb color (0-1) plus transparancy
-     * @param duration for this duration in seconds (0=forever)
-     */
-    void publishSphere(Vector3d &pos, const char* frame, const char* ns, int message_id, COLOR color,float radius = 0.01, int duration=0);
-
-    /**
-     * Publishes a cube visualization marker
-     * @param pos at this positon
-     * @param quat with this orientation
-     * @param frame in this frame
-     * @param ns namespace
-     * @param message_id a unique id
-     * @param rgda rgb color (0-1) plus transparancy
-     * @param duration for this duration in seconds (0=forever)
-     */
-    void publishCube(Vector3d &pos, Vector4d &quat, const char* frame, const char* ns, int message_id, COLOR color,float radius = 0.01, int duration=0);
-
-    /**
-     * Publishes a ray visualization marker
-     * @param pos at this positon
-     * @param dir direction
-     * @param frame in this frame
-     * @param message_id a unique id
-     * @param ns namespace
-     * @param color rgb color (0-1) plus transparancy
-     * @param duration for this duration in seconds (0=forever)
-     */
-    void publishRay(Vector3d &pos, Vector3d &dir, const char* frame, const char* ns, int message_id, COLOR color, int duration=0);
-    /**
-     * Publishes a text message marker
-     * @param pos at this positon
-     * @param text with this text
-     * @param frame in this frame
-     * @param ns namespace
-     * @param message_id a unique id
-     * @param color rgb color (0-1) plus transparancy
-     * @param duration for this duration in seconds (0=forever)
-     * @param height height of the text
-     */
-    void publishText(Vector3d &pos, const char *text, const char *frame, const char *ns, int message_id, COLOR color, int duration, float height);
 public:
     vector<int> pose_correction_sensors;
 private:
--- src/TrackedObject.cpp
+++ src/TrackedObject.cpp
@@ -11,7 +11,6 @@ TrackedObject::TrackedObject() {
     }
     nh = ros::NodeHandlePtr(new ros::NodeHandle);
 
-    visualization_pub = nh->advertise<visualization_msgs::Marker>("visualization_marker", 100);
     sensor_location_pub = nh->advertise<roboy_communication_middleware::DarkRoomSensor>("/roboy/middleware/DarkRoom/sensor_location", 1);
 
     if (const char* env_p = getenv("DARKROOM_CALIBRATED_OBJECTS")) {
@@ -605,7 +604,8 @@ void TrackedObject::trackSensors() {
                 ROS_INFO_THROTTLE(1, "sensor %d not active", sensor.first);
             }
         }
-        sensor_location_pub.publish(msg);
+        if(msg.ids.size()>0)
+            sensor_location_pub.publish(msg);
     }
 }
 
@@ -886,143 +886,4 @@ void TrackedObject::clearAll() {
 int TrackedObject::getMessageID(int type, int sensor, bool lighthouse) {
     return trackeObjectInstance * 5 * sensors.size() +
            type * (sensors.size() * NUMBER_OF_LIGHTHOUSES) + sensors.size() * lighthouse + sensor;
-}
-
-void TrackedObject::publishMesh(Vector3d &pos, Vector4d& orientation, const char* modelname,
-                                const char *frame, const char *ns, int message_id, int duration) {
-    visualization_msgs::Marker mesh;
-    mesh.header.frame_id = frame;
-    mesh.ns = ns;
-    mesh.type = visualization_msgs::Marker::MESH_RESOURCE;
-    mesh.color.r = 1.0f;
-    mesh.color.g = 1.0f;
-    mesh.color.b = 1.0f;
-    mesh.color.a = 0.5;
-    mesh.scale.x = 1.0;
-    mesh.scale.y = 1.0;
-    mesh.scale.z = 1.0;
-    mesh.lifetime = ros::Duration(duration);
-    mesh.header.stamp = ros::Time::now();
-    mesh.action = visualization_msgs::Marker::ADD;
-    mesh.id = message_id;
-    mesh.pose.position.x = pos[0];
-    mesh.pose.position.y = pos[1];
-    mesh.pose.position.z = pos[2];
-    mesh.pose.orientation.x = orientation[0];
-    mesh.pose.orientation.y = orientation[1];
-    mesh.pose.orientation.z = orientation[2];
-    mesh.pose.orientation.w = orientation[3];
-    char meshpath[200];
-    sprintf(meshpath,"package://darkroom/calibrated_objects/models/%s.dae", modelname);
-    mesh.mesh_resource = meshpath;
-    visualization_pub.publish(mesh);
-}
-
-void TrackedObject::publishSphere(Vector3d &pos, const char *frame, const char *ns, int message_id, COLOR color,
-                                  float radius, int duration) {
-    visualization_msgs::Marker sphere;
-    sphere.header.frame_id = frame;
-    sphere.ns = ns;
-    sphere.type = visualization_msgs::Marker::SPHERE;
-    sphere.color.r = color.r;
-    sphere.color.g = color.g;
-    sphere.color.b = color.b;
-    sphere.color.a = color.a;
-    sphere.lifetime = ros::Duration(duration);
-    sphere.scale.x = radius;
-    sphere.scale.y = radius;
-    sphere.scale.z = radius;
-    sphere.action = visualization_msgs::Marker::ADD;
-    sphere.header.stamp = ros::Time::now();
-    sphere.id = message_id;
-    sphere.pose.position.x = pos(0);
-    sphere.pose.position.y = pos(1);
-    sphere.pose.position.z = pos(2);
-    sphere.pose.position.z = pos(2);
-    sphere.pose.orientation.x = 1;
-    sphere.pose.orientation.y = 0;
-    sphere.pose.orientation.z = 0;
-    sphere.pose.orientation.w = 0;
-    visualization_pub.publish(sphere);
-}
-
-void TrackedObject::publishCube(Vector3d &pos, Vector4d &quat, const char *frame, const char *ns, int message_id, COLOR color,
-                                  float radius, int duration) {
-    visualization_msgs::Marker cube;
-    cube.header.frame_id = frame;
-    cube.ns = ns;
-    cube.type = visualization_msgs::Marker::CUBE;
-    cube.color.r = color.r;
-    cube.color.g = color.g;
-    cube.color.b = color.b;
-    cube.color.a = color.a;
-    cube.lifetime = ros::Duration(duration);
-    cube.scale.x = radius;
-    cube.scale.y = radius;
-    cube.scale.z = radius;
-    cube.action = visualization_msgs::Marker::ADD;
-    cube.header.stamp = ros::Time::now();
-    cube.id = message_id;
-    cube.pose.position.x = pos(0);
-    cube.pose.position.y = pos(1);
-    cube.pose.position.z = pos(2);
-    cube.pose.orientation.x = quat(0);
-    cube.pose.orientation.y = quat(1);
-    cube.pose.orientation.z = quat(2);
-    cube.pose.orientation.w = quat(3);
-    visualization_pub.publish(cube);
-}
-
-void
-TrackedObject::publishRay(Vector3d &pos, Vector3d &dir, const char *frame, const char *ns, int message_id, COLOR color,
-                          int duration) {
-    visualization_msgs::Marker arrow;
-    arrow.ns = ns;
-    arrow.type = visualization_msgs::Marker::ARROW;
-    arrow.color.r = color.r;
-    arrow.color.g = color.g;
-    arrow.color.b = color.b;
-    arrow.color.a = color.a;
-    arrow.lifetime = ros::Duration(duration);
-    arrow.scale.x = 0.005;
-    arrow.scale.y = 0.03;
-    arrow.scale.z = 0.03;
-    arrow.action = visualization_msgs::Marker::ADD;
-    arrow.header.stamp = ros::Time::now();
-
-    arrow.header.frame_id = frame;
-    arrow.id = message_id;
-    arrow.points.clear();
-    geometry_msgs::Point p;
-    p.x = pos(0);
-    p.y = pos(1);
-    p.z = pos(2);
-    arrow.points.push_back(p);
-    p.x += dir(0);
-    p.y += dir(1);
-    p.z += dir(2);
-    arrow.points.push_back(p);
-    visualization_pub.publish(arrow);
-}
-
-void TrackedObject::publishText(Vector3d &pos, const char *text, const char *frame, const char *ns, int message_id, COLOR color,
-                                int duration, float height) {
-    visualization_msgs::Marker text_msg;
-    text_msg.header.frame_id = frame;
-    text_msg.ns = ns;
-    text_msg.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    text_msg.color.r = color.r;
-    text_msg.color.g = color.g;
-    text_msg.color.b = color.b;
-    text_msg.color.a = color.a;
-    text_msg.lifetime = ros::Duration(duration);
-    text_msg.scale.z = height;
-    text_msg.action = visualization_msgs::Marker::ADD;
-    text_msg.header.stamp = ros::Time::now();
-    text_msg.id = message_id;
-    text_msg.pose.position.x = pos(0);
-    text_msg.pose.position.y = pos(1);
-    text_msg.pose.position.z = pos(2);
-    text_msg.text = text;
-    visualization_pub.publish(text_msg);
 }
